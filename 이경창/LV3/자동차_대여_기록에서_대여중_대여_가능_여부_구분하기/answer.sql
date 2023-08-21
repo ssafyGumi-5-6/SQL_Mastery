2f0619033af6ec856515087d9d2db660595bd660
이경창 <lkc263@naver.com>
2023-08-21 12:17:11 +0900

    프로그래머스 sql

---
+++ 이경창/LV3/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기/answer.sql
@@ -0,0 +1,22 @@
+-- mysql
+SELECT CAR_ID, (CASE
+                    WHEN CAR_ID IN (SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                                    WHERE '2022-10-16' BETWEEN START_DATE AND END_DATE)
+                        THEN '대여중'
+                    ELSE '대여 가능'
+    END) AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
+
+
+-- oracle
+SELECT CAR_ID, (CASE
+                    WHEN CAR_ID IN (SELECT CAR_ID FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                                    WHERE '2022-10-16' BETWEEN TO_CHAR(START_DATE, 'YYYY-mm-dd') AND TO_CHAR(END_DATE, 'YYYY-mm-dd'))
+                        THEN '대여중'
+                    ELSE '대여 가능'
+    END) AVAILABILITY
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY CAR_ID
+ORDER BY CAR_ID DESC;
